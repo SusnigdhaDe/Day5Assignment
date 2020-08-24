a65c45b806e2373fb5eda258b5e681bdc23cd7cc
Susnigdha De <susnigdha.de@gmail.com>
2020-08-24 07:45:19 +0530

    Read a single digit number and write the number in word

---
+++ read_single_digit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+#Read a single digit number and write the number in word
+
+read -p "Enter a single digit number" n
+
+if [ $n -eq 0 ]
+then echo Zero
+elif [ $n -eq 1 ]
+then echo One
+elif [ $n -eq 2 ]
+then echo Two
+elif [ $n -eq 3 ]
+then echo Three
+elif [ $n -eq 4 ]
+then echo Four
+elif [ $n -eq 5 ]
+then echo Five
+elif [ $n -eq 6 ]
+then echo Six
+elif [ $n -eq 7 ]
+then echo Seven
+elif [ $n -eq 8 ]
+then echo Eight
+elif [ $n -eq 9 ]
+then echo Nine
+else
+echo Invalid Entry
+fi
+
